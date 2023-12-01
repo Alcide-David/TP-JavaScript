10d45d85bfc3fe324d1fa3691c7d02451863cf23
Alcide-David <152320912+Alcide-David@users.noreply.github.com>
2023-11-30 21:27:15 -0500

    Update Miniskil.js

--- TP 1/Miniskil.js
+++ TP 1/Miniskil.js
@@ -6,7 +6,7 @@ while(moMajiskil !== moMajiskil.toUpperCase() || !moMajiskil.trim()){
     console.info(" ERE : Rezon yo ka :")
     console.info("    --> Yon let oswa mo a pat an majiskil")
     console.info("    --> Varyab lan vid")
-    moMajiskil = prompt(" Mo a oswa yon let ladan pat an Majiskil (Oswa cham rete vide), eseye anko : ")
+    moMajiskil = prompt(" Mo a oswa yon let ladan pat an Majiskil (Oswa chan rete vide), eseye anko : ")
 }
 let moMiniskil = moMajiskil.toLowerCase();
 
